24c838be3785a19ad651deb2a49b2fa182f4925a
Collin Koldoff <collinkoldoff@gmail.com>
2026-05-03 14:17:58 -0500

    Implemented local storage for theme

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,7 +1,9 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+
+const THEME_KEY = 'theme';
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +11,21 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {
+export class Navbar implements OnInit {
   currentPath = window.location.pathname;
   auth = inject(AuthService);
-  isDark = signal(document.documentElement.getAttribute('data-theme') !== 'light');
+  isDark = signal(localStorage.getItem(THEME_KEY) === 'dark');
   menuOpen = signal(false);
+
+  ngOnInit() {
+    const state = this.isDark() ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', state);
+  }
 
   toggleTheme() {
     const next = this.isDark() ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', next);
     this.isDark.set(next === 'dark');
+    localStorage.setItem(THEME_KEY, next);
   }
 }
