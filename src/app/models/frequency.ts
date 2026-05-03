c2220f5df22f95b738b6e604cf43ee9ba7a29505
Collin Koldoff <collinkoldoff@gmail.com>
2026-05-03 17:52:24 -0500

    Removed unused Frequency enum

--- src/app/models/frequency.ts
+++ src/app/models/frequency.ts
@@ -4,11 +4,3 @@ export interface FrequencyResponse {
   daysInterval: number | null;
   monthsInterval: number | null;
 }
-
-export enum Frequency {
-  Weekly = 1,
-  Biweekly = 2,
-  Monthly = 3,
-  SemiAnnually = 4,
-  Annually = 5,
-}
