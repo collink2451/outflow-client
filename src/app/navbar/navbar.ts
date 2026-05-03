02815fa4c9b50a7e7854cb6f77b6f40cf7309e82
Collin Koldoff <collinkoldoff@gmail.com>
2026-05-02 20:20:24 -0500

    Added highlight to nav links

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,13 +1,16 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, NgTemplateOutlet],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  currentPath = window.location.pathname;
   auth = inject(AuthService);
   isDark = signal(document.documentElement.getAttribute('data-theme') !== 'light');
   menuOpen = signal(false);
