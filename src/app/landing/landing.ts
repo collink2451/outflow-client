622224a048f43baaa1815c050c5f11a7d8c2681f
Collin Koldoff <collinkoldoff@gmail.com>
2026-05-03 14:02:39 -0500

    Fixed missing scss in landing

--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-landing',
   imports: [RouterLink],
   templateUrl: './landing.html',
+  styleUrl: './landing.scss',
 })
 export class Landing {
   auth = inject(AuthService);
-  private router = inject(Router);
 }
