64a126fd91e93289d6c3ab979f427438b5e01f62
Collin Koldoff <collinkoldoff@gmail.com>
2026-06-24 19:16:00 -0500

    Prevent new user sign up

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { NgTemplateOutlet } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { ToastService } from '../services/toast.service';
 
 const THEME_KEY = 'theme';
 
@@ -13,12 +14,19 @@ const THEME_KEY = 'theme';
 export class Navbar implements OnInit {
   currentPath = window.location.pathname;
   auth = inject(AuthService);
+  toast = inject(ToastService);
   isDark = signal(localStorage.getItem(THEME_KEY) !== 'light');
   menuOpen = signal(false);
 
   ngOnInit() {
     const state = this.isDark() ? 'dark' : 'light';
     document.documentElement.setAttribute('data-theme', state);
+
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('error') === 'signup_disabled') {
+      this.toast.show('Sign-ups are disabled. Contact the administrator to create an account.', 'error');
+      history.replaceState(null, '', window.location.pathname);
+    }
   }
 
   toggleTheme() {
